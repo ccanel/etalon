4e2777e2ee747e8a33fde7b26f547561b86b9d41
Shawn Chen <sschenshawn@gmail.com>
2021-01-04 12:24:16 -0800

    Force docker to build image without using cache. This ensures that libVT.so with new TDF is picked up.

--- etc/python_config.py
+++ etc/python_config.py
@@ -115,7 +115,7 @@ DOCKER_IMAGE = 'etalon'
 DOCKER_CLEAN = 'sudo docker ps -q | xargs sudo docker stop -t 0 ' \
                '2> /dev/null; ' \
                'sudo docker ps -aq | xargs sudo docker rm 2> /dev/null'
-DOCKER_BUILD = 'sudo docker build -t %s -f ' \
+DOCKER_BUILD = 'sudo docker build --no-cache -t %s -f ' \
 '/etalon/vhost/etalon.dockerfile /etalon/vhost/' % DOCKER_IMAGE
 DOCKER_LOCAL_IMAGE_PATH = '/etalon/vhost/etalon.img'
 DOCKER_SAVE = 'sudo docker save -o {img} etalon && sudo chown ' \
